bc1cf61bcc72ec20390af060ed1935cb8b94e94c
anh1banan1 <72141667+anh1banan1@users.noreply.github.com>
2020-10-15 17:09:09 +0200

    Create variable pokemonList
    Add 3 pokemon objects with 5 keys.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-alert("What's your favorite food?");
-
-let favoriteFood = "Italian";
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Chikorita',
+        type: 'grass',
+        height: 2,
+        weakness: ['fire', 'flying', 'ice'],
+        evolutions: ['Bayleaf', 'Meganium']
+    },
+    {
+        name: 'Cyndaquil',
+        type: 'fire',
+        height: 1,
+        weakness: ['water', 'ground', 'rock'],
+        evolutions: ['Quilava', 'Typhlosion']
+    },
+    {
+        name: 'Totodile',
+        type: 'water',
+        height: 2,
+        weakness: ['gradd', 'electric'],
+        evolutions: ['Croconaw', 'Feraligatr']
+    }
+]
