a483ce4ea2733ae5e48facc13d78656375d8123d
anh1banan1 <72141667+anh1banan1@users.noreply.github.com>
2020-10-22 13:28:09 +0200

    Update script.js
    Added forEach comment

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,7 @@ let pokemonList = [
     }
 ]
 
+// FOR LOOP
 // for (let i = 0; i < pokemonList.length; i++) {
 //     if (pokemonList[i].weight > 20) {
 //         document.write(pokemonList[i].name + " (weight: " + pokemonList[i].weight + ") - Wow, that's heavy!<br>")
@@ -31,7 +32,7 @@ let pokemonList = [
 //     }
 // }
 
-
+// forEACH LOOP
 pokemonList.forEach(function (pokemon){
     if (pokemon.weight >20) {
         document.write(pokemon.name + "(weight: " + pokemon.weight + ") - Wow, that's heavy!<br>")
